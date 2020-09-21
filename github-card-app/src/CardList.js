d1f378cad7435f7e80ffb8cdedf0edbd044e6327
yue <yue.liu@authright.com>
2020-09-21 00:18:25 -0400

    git-cards-app

--- github-card-app/src/CardList.js
+++ github-card-app/src/CardList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from './Card';
 
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"}
-]
+// const testData = [
+//   {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"}
+// ]
 
 const CardList = (props) => {
   return(
     <div>
-      {props.profiles.map(profile => <Card {...profile}/>)}
+      {props.profiles.map(profile => <Card key={profile.id} {...profile}/>)}
     </div>
   )
 }
--- github-card-app/src/Form.js
+++ github-card-app/src/Form.js
@@ -1,21 +1,32 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
+import axios from 'axios';
+// an agent module
 
-const Form = () => {
+const Form = (props) => {
 
-  const [username, setUsername] = useState({
-    userName: ''
-  });
+  const [username, setUsername] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(
-      userNameInput.current.value
-    )
+  // const usernameInput = useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); // to avoid refresh
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    // console.log(
+    //   // usernameInput.current.value
+    //   // username
+    //   response
+    // )
+    props.onSubmit(response.data);
+  };
+
+  const handleChange = (event) => {
+    setUsername(event.target.value)
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={userName} placeholder="Github username" required/>
+      <input type="text" value={username} onChange={handleChange} placeholder="Github username" required/>
+      {/* <input type="text" ref={usernameInput} placeholder="Github username" required/> */}
       <button>Add</button>
     </form>
   );
