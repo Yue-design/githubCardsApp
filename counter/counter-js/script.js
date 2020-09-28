4f135cd4db031fce9cb7585d7253a4d2d06e0b36
yue <yue.liu@authright.com>
2020-09-28 18:53:30 -0400

    refactor-less-functions

--- counter/counter-js/script.js
+++ counter/counter-js/script.js
@@ -1,37 +1,43 @@
 let count = 0;
-function incrementOne() {
-  if (document.getElementById("add1")) {
-    count += 1;
-    document.getElementById("output").value = count;
-  };
-  // if (document.getElementById("add5")) {
-  // 	count += 5;
-  // 	document.getElementById("output").value = count;
-  // };
-  // if (document.getElementById("add10")) {
-  // 	count += 10;
-  // 	document.getElementById("output").value = count;
-  // };
-  // if (document.getElementById("add100")) {
-  // 	count += 100;
-  // 	document.getElementById("output").value = count;
-  // };
-};
-function incrementFive() {
-  if (document.getElementById("add5")) {
-    count += 5;
+function increamentAmount(amt) {
+  if (document.getElementById("add"+amt)) {
+    count += amt;
     document.getElementById("output").value = count;
   };
 };
-function incrementTen() {
-  if (document.getElementById("add10")) {
-    count += 10;
-    document.getElementById("output").value = count;
-  };
+
+// closure is not applicabe at this stage
+function addAmount(amt) {
+  return count+amt;
 };
-function incrementOneHundred() {
-  if (document.getElementById("add100")) {
-    count += 100;
-    document.getElementById("output").value = count;
-  };
-};
+const addOne = addAmount(1);
+const addFive = addAmount(5);
+const addTen = addAmount(10);
+const addHundred = addAmount(100);
+console.log(addOne, addFive, addTen, addHundred);
+
+// from four different functions to a general funciton
+// function incrementOne() {
+//   if (document.getElementById("add1")) {
+//     count += 1;
+//     document.getElementById("output").value = count;
+//   };
+// };
+// function incrementFive() {
+//   if (document.getElementById("add5")) {
+//     count += 5;
+//     document.getElementById("output").value = count;
+//   };
+// };
+// function incrementTen() {
+//   if (document.getElementById("add10")) {
+//     count += 10;
+//     document.getElementById("output").value = count;
+//   };
+// };
+// function incrementOneHundred() {
+//   if (document.getElementById("add100")) {
+//     count += 100;
+//     document.getElementById("output").value = count;
+//   };
+// };
