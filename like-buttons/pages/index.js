69f7a86ecc764294a790b14be1e7ecf9865c3d68
yue <yue.liu@authright.com>
2020-10-10 23:10:10 -0400

    buttons-hidden

--- like-buttons/pages/index.js
+++ like-buttons/pages/index.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import LikeButton from '../pages/_app';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import LikeButton from "../pages/_app";
+import Reacth, { useState } from "react";
 
 export default function Home(props) {
   const count = 1;
+  const [hidden, setHidden] = useState(true);
+  const text = "hello";
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,11 +17,14 @@ export default function Home(props) {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Reference <a href="https://gist.github.com/gaearon/faa67b76a6c47adbab04f739cba7ceda">is here!</a>
+          Reference{" "}
+          <a href="https://gist.github.com/gaearon/faa67b76a6c47adbab04f739cba7ceda">
+            is here!
+          </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -27,13 +34,26 @@ export default function Home(props) {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
           {/* <LikeButton></LikeButton> */}
-          <button onClick={e=>console.log(`You liked article number ${count}.`)}>Like</button>
+          <button
+            onClick={e => console.log(`You liked article number ${count}.`)}
+          >
+            Like
+          </button>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-          <button>Like</button>
+          <button onClick={() => setHidden(false)}>Like</button>
+
+          <span>
+            {text}
+            {hidden ? (
+              <a onClick={() => setHidden(false)}>read more</a>
+            ) : (
+              <a onClick={() => setHidden(true)}>read less</a>
+            )}
+          </span>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
@@ -42,7 +62,7 @@ export default function Home(props) {
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
-          <button>Like</button>
+          <button onClick={() => {console.log("here")}}>Like</button>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -63,10 +83,10 @@ export default function Home(props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
