9d564da9d38c119362d9c57a81b6313f18cc8b8d
yue <yue.liu@authright.com>
2020-10-24 00:41:15 -0400

    markdown-render

--- lists/markdown-editor/src/App.js
+++ lists/markdown-editor/src/App.js
@@ -1,32 +1,38 @@
-import React, {useState} from 'react';
-import Remarkable from 'remarkable-react';
-import './App.css';
+import React, { useState } from "react";
+import Remarkable from "remarkable-react";
+import "./App.css";
+
+function createMarkup() {
+  return { __html: "First &middot; Second" };
+}
+
+// function getRawMarkup() {
+//   let md = new Remarkable();
+//   return { __html: md.render() };
+// }
 
 function MarkdownEditor(props) {
-
-  let md = new Remarkable();
-
   const [state, setState] = useState({
-    value: 'Hello World!'
+    value: "Hello, **world**!"
   });
 
-  const handleChange = (e) => {
-    setState({value: e.target.value});
-  };
-
-  const getRawMarkup = () => {
-    return;
-    // let md = new Remarkable();
-    // return {__html: md.render()};
+  const handleChange = e => {
+    setState({ value: e.target.value });
   };
 
   return (
     <div className="App">
       <h3>Input</h3>
       <label htmlFor="markdown-content">Enter some markdown</label>
-      <textarea id="markdown-content" onChange={handleChange} defaultValue={state.value}/>
+      <textarea
+        id="markdown-content"
+        type="text"
+        onChange={handleChange}
+        defaultValue={state.value}
+      />
       <h3>Output</h3>
-      <div dangerouslySetInnerHTML={getRawMarkup()}/>
+      {/* <div dangerouslySetInnerHTML={getRawMarkup()} /> */}
+      <div dangerouslySetInnerHTML={createMarkup()} />
       <div>{state.value}</div>
     </div>
   );
--- lists/markdown-editor/src/index.js
+++ lists/markdown-editor/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import MarkdownEditor from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <MarkdownEditor />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
